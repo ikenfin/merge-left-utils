9d355a790d37c247adaf19071aa2cf97310bdec6
Sergey Tominoff <ikenfin@gmail.com>
2021-03-18 12:01:50 +0400

    export new functions

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export type { DeepPartial } from './src/util'
 export * from './src/merge-left'
 export * from './src/merge-left-keys'
+export * from './src/merge-left-dropping'
+export * from './src/merge-left-except'
