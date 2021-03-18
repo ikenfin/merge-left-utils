5f95254b0d83b4393a849ecbd461b17f953a3100
Sergey Tominoff <ikenfin@gmail.com>
2021-03-18 11:20:45 +0400

    added mergeLeftExcept function to optionally skip replacing source keys from target

---
+++ src/merge-left-except.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from './util'
+import { mergeLeftKeys } from './merge-left-keys'
+
+/*
+  Shortcut function
+  Replace only existing fields in {a} with {b} skipping {b} keys from {keys}
+
+  ex: (['a'], { a: 0, b: 0 }, { a: 1, x: 1 }) -> { b: 0 }
+*/
+export function mergeLeftExcept<T extends Record<string, any>> (
+  skippingKeys: string[],
+  source: T,
+  target?: DeepPartial<T>
+): T {
+  return mergeLeftKeys<T>(
+    Object.keys(source),
+    source,
+    target,
+    (keyOfSource) => skippingKeys.indexOf(keyOfSource) === -1
+  )
+}
---
+++ test/merge-left-except.test.ts
@@ -0,0 +1,34 @@
+import { mergeLeftKeys } from '../src/merge-left-keys'
+import { mergeLeft } from '../src/merge-left'
+import { mergeLeftExcept } from '../src/merge-left-except';
+
+const source: Record<string, string> = {
+  a: 'a',
+  b: 'b',
+  c: 'c',
+  d: 'd',
+  e: 'e'
+}
+
+const target: Record<string, string> = {
+  a: 'a from target',
+  b: 'b from target',
+  c: 'c from target',
+  e: 'e from target',
+  f: 'f from target'
+}
+
+test('Check is empty skippingKeys returns same result with mergeLeft', () => {
+  expect(mergeLeft(source, null)).toEqual(mergeLeftExcept([], source, null))
+  expect(mergeLeft(source, target)).toEqual(mergeLeftExcept([], source, target))
+})
+
+test('Check is skipped keys not replaced from B', () => {
+  expect(mergeLeftExcept(['a', 'b'], source, target)).toEqual({
+    a: source.a,
+    b: source.b,
+    c: target.c,
+    d: source.d,
+    e: target.e
+  })
+})
