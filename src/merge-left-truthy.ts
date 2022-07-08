51c5d3d3c4186f1c7b5143f105628756c0ce7411
Tominov Sergey <ikenfin@gmail.com>
2022-07-08 16:55:07 +0400

    add merge-left-truthy func

---
+++ src/merge-left-truthy.ts
@@ -0,0 +1,29 @@
+import type { DeepPartial } from './util'
+import { mergeLeftKeys } from './merge-left-keys'
+import { alwaysTrue } from './util'
+
+/*
+  Shortcut function
+  Replace only existing fields in {a} with {b}
+  When field from {b} is truthy
+
+  ex: ({ a: 0, b: 0 }, { a: 1, x: 1 }) -> { a: 1, b: 0 }
+*/
+export function mergeLeftTruthy<T extends Record<string, any>> (
+  source: T,
+  target?: DeepPartial<T>,
+  customLogic: (
+    key: string,
+    source: T,
+    target: DeepPartial<T>
+  ) => boolean = alwaysTrue
+): T {
+  return mergeLeftKeys<T>(
+    Object.keys(source),
+    source,
+    target,
+    (key, source, target) => {
+      return !!target[key] && customLogic(key, source, target)
+    }
+  )
+}
---
+++ test/merge-left-truthy.test.ts
@@ -0,0 +1,61 @@
+import { mergeLeftTruthy } from './../dist'
+
+const source: Record<string, string> = {
+  a: 'source:a',
+  b: 'source:b',
+  c: 'source:c',
+  e: 'source:e',
+  f: 'source:f',
+  g: 'source:g',
+  h: 'source:h',
+  i: 'source:i',
+  j: 'source:j',
+  k: 'source:k',
+  l: 'source:l',
+  m: 'source:m',
+  o: 'source:o',
+  p: 'source:p',
+  q: 'source:q',
+  r: 'source:r',
+  s: 'source:s',
+  t: 'source:t',
+  u: 'source:u'
+}
+
+const target: Record<string, any> = {
+  a: 'a from target',
+  b: 'b from target',
+  c: 'c from target',
+  e: 'e from target',
+  f: undefined,
+  g: null,
+  h: 0,
+  i: -1,
+  j: [],
+  k: {},
+  l: false,
+  m: -0,
+  o: '',
+  p: '',
+  q: ``,
+  r: NaN,
+  s: () => {},
+  t: 'false',
+  u: 'null'
+}
+
+test('Check are fields repalaced only with truthy target fields', () => {
+  expect(mergeLeftTruthy(source, target)).toEqual({
+    ...source,
+    a: target.a,
+    b: target.b,
+    c: target.c,
+    e: target.e,
+    i: target.i,
+    j: target.j,
+    k: target.k,
+    s: target.s,
+    t: target.t,
+    u: target.u
+  })
+})
